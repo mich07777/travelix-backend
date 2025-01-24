a158dbfe7156576e687964c0904c942232c1ae4e
mich07777 <abishekrosario@gmail.com>
2025-01-24 11:23:24 +0530

    travelix-backend

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import mysql from "mysql";
 import http from "http";
-import { error } from "console";
 
 const app = express();
 const server = http.createServer(app);
@@ -12,7 +11,7 @@ app.use(cors({
     origin: "*"
 }))
 
-app.use(express.json({ limit: '100mb' }));
+app.use(express.json({ limit: '10mb' }));
 
 const connection = mysql.createConnection({
     host: "db4free.net",
@@ -31,36 +30,35 @@ connection.connect((error) => {
     }
 })
 
-// url-http://localhost:4000/api/add/destination
-
-// payload{
-//     name:""
-//     image:""
-//     count:""
+// URL: http://localhost:4000/api/destinaiton/add
+// Payload:
+// {
+//     name : "",
+//     image : "",
+//     count : ""
 // }
 
-app.post("/api/add/destination",(request,response)=> {
-   const incomingValue = request.body;
+app.post("/api/destinaiton/add", (request, response) => {
+    const incomingValue = request.body;
 
-   const sqlQuery = `INSERT INTO sks_destination (name,image,count,location) VALUES ('${incomingValue.name}','${incomingValue.image}','${incomingValue.count}','${incomingValue.location}')`;
+    const sqlQuery = `INSERT INTO sks_destination (name, image, count, location) VALUES ('${incomingValue.name}', '${incomingValue.image}', '${incomingValue.count}', '${incomingValue.location}')`;
 
-   connection.query(sqlQuery,(error,result) => {
-    if(error){
-        response.status(500).send("please contact Admin");
-    }
-    else{
-        response.status(200).send("Destination has been added");
-    }
-   })
-})
+    connection.query(sqlQuery, (error, result) => {
+        if(error){
+            response.status(500).send("Pls contact Admin");
+        }
+        else{
+            response.status(200).send("Destination has been added");
+        }
+    })  
+});
 
+app.get("/api/destination/load", (request, response) => {
+    const sqlQuery = `SELECT * FROM sks_destination`;
 
-app.get("/api/desttination/load",(request,response) => {
-    const sqlQuery =`SELECT * FROM sks_destination`;
-
-    connection.query(sqlQuery,(error,result) => {
-        if(error) {
-            response.status(500).send("Please Conatact Admin");
+    connection.query(sqlQuery, (error, result) => {
+        if(error){
+            response.status(500).send("Pls contact Admin");
         }
         else{
             response.status(200).send(result);
@@ -68,68 +66,98 @@ app.get("/api/desttination/load",(request,response) => {
     })
 })
 
-app.delete("/api/destination/delete/:id",(request,response) => {
+app.delete("/api/destination/delete/:id", (request, response) => {
     const id = request.params.id;
-    const sqlQuery =`DELETE FROM sks_destination WHERE id=${id}`;
+    const sqlQuery = `DELETE FROM sks_destination WHERE id=${id}`;
 
-    connection.query(sqlQuery,(error,result) => {
+    connection.query(sqlQuery, (error, result) => {
         if(error){
-            response.status(500).send("Please Contact Admin");
+            response.status(500).send("Pls contact Admin");
         }
         else{
-            response.status(200).send("Destination has been Deleted");
+            response.status(200).send("Record has been deleted");
         }
-       })
+    })
+})
+
+app.get("/api/destination/search" , (request, response) => {
+    const destination = request.query.destination;
+    const location = request.query.location;
+    
+    let sqlQuery = `SELECT * FROM sks_destination WHERE name LIKE '${destination}' AND location LIKE '${location}'`;
+
+    if(destination === '' && location === ''){
+        response.status(400).send("Invalid Search Data");
+    }
+    else if(destination !== '' && location === ''){
+        sqlQuery =  `SELECT * FROM sks_destination WHERE name LIKE '${destination}'`
+    }
+    else if(destination === '' && location !== ''){
+        sqlQuery =  `SELECT * FROM sks_destination WHERE location LIKE '${location}'`
+    }
+    
+    connection.query(sqlQuery, (error, result) => {
+        if(error){
+            response.status(500).send("Pls contact Admin");
+        }
+        else{
+            response.status(200).send(result);
+        }
+    })
+    
 })
 
 
-app.post("/api/add/hotels",(request,response)=> {
+app.post("/api/hotel/add", (request, response) => {
     const incomingValue = request.body;
     const actualPrice = parseInt(incomingValue.price);
- 
-    const sqlQuery = `INSERT INTO sks_hotels (name,location,price,image,destination) VALUES ('${incomingValue.name}','${incomingValue.location}','${actualPrice}','${incomingValue.image}','${incomingValue.destination}')`;
- 
-    connection.query(sqlQuery,(error,result) => {
-     if(error){
-         response.status(500).send("please contact Admin");
-     }
-     else{
-         response.status(200).send("Hotel has been added");
-     }
-    })
- })
 
+    const sqlQuery = `INSERT INTO sks_hotels (name, location, price, image, destination) VALUES ('${incomingValue.name}', '${incomingValue.location}', '${actualPrice}', '${incomingValue.image}', '${incomingValue.destination}')`;
 
- app.get("/api/hotel/load",(request,response) => {
-    const sqlQuery =`SELECT * FROM sks_hotels`;
+    connection.query(sqlQuery, (error, result) => {
+        if(error){
+            response.status(500).send("Pls contact Admin");
+        }
+        else{
+            response.status(200).send("Hotel has been added");
+        }
+    })  
+});
 
-    connection.query(sqlQuery,(error,result) => {
-        if(error) {
-            response.status(500).send("Please Conatact Admin");
+app.get("/api/hotel/search" , (request, response) => {
+    const destination = request.query.destination;
+    
+    let sqlQuery = `SELECT * FROM sks_hotels WHERE destination LIKE '${destination}'`;
+
+    if(destination === ''){
+        response.status(400).send("Invalid Search Data");
+    }
+    
+    
+    connection.query(sqlQuery, (error, result) => {
+        if(error){
+            response.status(500).send("Pls contact Admin");
         }
         else{
             response.status(200).send(result);
         }
     })
+    
 })
-
-app.delete("/api/hotel/delete/:id",(request,response) => {
-    const id = request.params.id;
-    const sqlQuery =`DELETE FROM sks_hotels WHERE id=${id}`;
-
-    connection.query(sqlQuery,(error,result) => {
-        if(error){
-            response.status(500).send("Please Contact Admin");
-        }
-        else{
-            response.status(200).send("Destination has been Deleted");
-        }
-       })
-})
-
-
 
 const port = 4000;
 server.listen(port, () => {
     console.log("Node JS is running on port 4000");
 })
+
+
+/* 
+
+ALTER TABLE table_name ADD columName dataType;
+
+USE travelix;
+CREATE TABLE sks_hotels (name varchar(255), location varchar(255), price int, image LONGTEXT, destination varchar(255), id int NOT NULL AUTO_INCREMENT, PRIMARY KEY(id));
+
+USE travelix;
+SELECT * FROM sks_destination WHERE name LIKE 'Beach' AND location LIKE 'Mumbai';
+*/
